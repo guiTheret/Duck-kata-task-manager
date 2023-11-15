2a90930da8d660576f3be6af12c9b3207bf374fa
guiTheret <73123940+guiTheret@users.noreply.github.com>
2023-11-15 10:54:05 +0100

    up

--- index.js
+++ index.js
@@ -1,54 +1,62 @@
-const prompt = require("prompt-sync")();
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const data = [];
 
-// infinite loop to keep the program running
-while (true) {
-  // ask user for input, input can be - + x o q
-  const input = prompt(
-    "What do you want to do? \n + <description> \n - <id> \n x <id> \n o <id> \n q"
-  );
+const getPrompt = () => {
+  return new Promise((resolve) => {
+    rl.question(
+      "What do you want to do?\n+ <description>\n- <id>\nx <id>\no <id>\nq\n",
+      (input) => {
+        resolve(input);
+      }
+    );
+  });
+};
 
-  // check input
-  if (input?.startsWith("+")) {
-    // add task
-    const task = {
-      id: data.length + 1,
-      description: input.slice(1),
-      status: "todo",
-    };
-    data.push(task);
-  } else if (input?.startsWith("-")) {
-    // remove task
-    const id = Number(input.slice(1));
-    const index = data.findIndex((task) => task.id === id);
-    // if index is -1, task is not found
-    if (index === -1) {
-      console.log("Task not found");
-      continue;
+const taskManager = async () => {
+  while (true) {
+    const input = await getPrompt();
+
+    if (input?.startsWith("+")) {
+      const task = {
+        id: data.length + 1,
+        description: input.slice(1),
+        status: "todo",
+      };
+      data.push(task);
+    } else if (input?.startsWith("-")) {
+      const id = Number(input.slice(1));
+      const index = data.findIndex((task) => task.id === id);
+      if (index === -1) {
+        console.log("Task not found");
+        continue;
+      }
+      data.splice(index, 1);
+    } else if (input?.startsWith("x")) {
+      const id = Number(input.slice(1));
+      const index = data.findIndex((task) => task.id === id);
+      if (index === -1) {
+        console.log("Task not found");
+        continue;
+      }
+      data[index].status = "done";
+    } else if (input?.startsWith("o")) {
+      const id = Number(input.slice(1));
+      const index = data.findIndex((task) => task.id === id);
+      if (index === -1) {
+        console.log("Task not found");
+        continue;
+      }
+      data[index].status = "todo";
+    } else if (input === "q") {
+      rl.close();
+      break;
     }
-    data.splice(index, 1);
-  } else if (input?.startsWith("x")) {
-    // set status to done
-    const id = Number(input.slice(1));
-    const index = data.findIndex((task) => task.id === id);
-    // if index is -1, task is not found
-    if (index === -1) {
-      console.log("Task not found");
-      continue;
-    }
-    data[index].status = "done";
-  } else if (input?.startsWith("o")) {
-    // set status to todo
-    const id = Number(input.slice(1));
-    const index = data.findIndex((task) => task.id === id);
-    // if index is -1, task is not found
-    if (index === -1) {
-      console.log("Task not found");
-      continue;
-    }
-    data[index].status = "todo";
-  } else if (input === "q") {
-    // quit program
-    break;
   }
-}
+};
+
+taskManager();
