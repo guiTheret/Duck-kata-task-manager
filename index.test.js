2c8a94a42298c9619ac652335b1fa2cf8ffc0076
guiTheret <73123940+guiTheret@users.noreply.github.com>
2023-11-15 11:15:49 +0100

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,13 +1,10 @@
+const { verifyInput } = require("./functions");
 const readline = require("readline");
-const { getPrompt } = require("./functions");
 
-describe("getPrompt", () => {
-  //check if first character is + - x o or q, if not return error
-  test("should return error if first character is not + - x o or q", async () => {
-    readline.createInterface().question.mockImplementationOnce((_, cb) => {
-      cb("a");
-    });
-    const result = await getPrompt();
-    expect(result).toBe("Error");
+describe("verifyInput", () => {
+  // Check if the first character is + - x o or q; if not, return an error
+  test("should return error if the first character is not + - x o or q", async () => {
+    const res = verifyInput("a");
+    expect(res).toBe("Invalid input");
   });
 });
