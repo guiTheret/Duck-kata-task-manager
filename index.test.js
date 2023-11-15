da4e45f3a4b523d7e04266a4e27e51f61de9f86a
Enrique <73123851+F4l4X@users.noreply.github.com>
2023-11-15 12:11:45 +0100

    test

--- index.test.js
+++ index.test.js
@@ -10,11 +10,26 @@ describe("verifyInput", () => {
   });
 });
 
-
 describe("verifyInputIsValid", () => {
   // Check if the first character is + - x o or q; if not, return an error
   test("should return error if the first character is not + - x o or q", async () => {
     const res = verifyInput("+");
     expect(res).not.toBe("Invalid input");
+  });
+});
+
+describe("checkAddInput", () => {
+  test("should return error if list already in list", () => {
+    const testArray = [
+      {
+        id: 1,
+        description: "test",
+        status: "todo",
+      },
+    ];
+
+    const existingTask = "test";
+
+    expect(() => addTask([...testArray], existingTask)).toThrowError;
   });
 });
